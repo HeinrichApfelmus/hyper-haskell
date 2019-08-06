9b55a4d388bc9eba80ed2faeba2dfe8a0e9c19f7
Heinrich Apfelmus <apfelmus@quantentunnel.de>
2019-08-06 13:19:30 +0200

    Add support for `svg-builder` in `hyper-extra`

--- haskell/hyper-extra/src/Hyper/Extra.hs
+++ haskell/hyper-extra/src/Hyper/Extra.hs
@@ -2,6 +2,8 @@ module Hyper.Extra (
     -- * Synopsis
     -- | Visual representation for various data types.
     
+    -- * SVG
+    fromSvg,
     -- * Diagrams
     dia,
 
@@ -11,21 +13,27 @@ module Hyper.Extra (
 
 import Hyper
 
-import qualified Data.Text        as T
-import qualified Data.Text.Lazy   as TL
+import qualified Data.Text        as Text
+import qualified Data.Text.Lazy   as Text.Lazy
 
-import Diagrams.Prelude                   hiding (pre)
-import Diagrams.Backend.SVG
-import Graphics.Svg               as SVG
+import           Diagrams.Prelude                   hiding (pre)
+import           Diagrams.Backend.SVG
+import qualified Graphics.Svg
 
 import qualified Test.QuickCheck  as Q
 
 {-----------------------------------------------------------------------------
-    Integration of the `diagrams-svg` and `svg-builder` packages
+    Integration of the `svg-builder` and `diagrams-svg` spackages
 ------------------------------------------------------------------------------}
+-- | Render a SVG document.
+-- This assumes that the argument is indeed a complete SVG document,
+-- i.e. an @<SVG>@ tag.s
+fromSvg :: Graphics.Svg.Element -> Graphic
+fromSvg = html . Text.Lazy.toStrict . Graphics.Svg.renderText
+
+-- | Render a diagram via SVG.
 dia :: QDiagram SVG V2 Double Any -> Graphic
-dia = html . TL.toStrict . SVG.renderText
-    . renderDia SVG (SVGOptions (mkWidth 250) Nothing (T.pack "") [] True)
+dia = fromSvg . renderDia SVG (SVGOptions (mkWidth 250) Nothing (Text.pack "") [] True)
 
 {-----------------------------------------------------------------------------
     Integration of the `QuickCheck` package
@@ -34,4 +42,4 @@ hyperCheck :: Q.Testable prop => prop -> IO Graphic
 hyperCheck =
     fmap (pre . Q.output) . Q.quickCheckWithResult (Q.stdArgs{ Q.chatty = False })
 
-pre s = html . T.pack $ "<pre>" ++ s ++ "</pre>"
+pre s = html . Text.pack $ "<pre>" ++ s ++ "</pre>"
