1fff7cf2b5d78915756c19dff7bd7344e14117c3
Heinrich Apfelmus <apfelmus@quantentunnel.de>
2025-10-21 13:45:47 +0200

    Use absolute sizes in `dia`

--- haskell/hyper-extra/src/Hyper/Extra.hs
+++ haskell/hyper-extra/src/Hyper/Extra.hs
@@ -33,28 +33,43 @@ import qualified Graphics.Rendering.Chart.Backend.Diagrams as Chart
 ------------------------------------------------------------------------------}
 -- | Render a SVG document.
 -- This assumes that the argument is indeed a complete SVG document,
--- i.e. an @<SVG>@ tag.s
+-- i.e. an @<SVG>@ tag.
 fromSvg :: Graphics.Svg.Element -> Graphic
 fromSvg = html . Text.Lazy.toStrict . Graphics.Svg.renderText
 
 -- | Render a diagram via SVG.
+--
+-- Uses absolute units:
+-- one unit in the diagram corresponds to @1px@ in the rendered image.
+--
+-- To change the scale in your diagram before rendering, use 'scale'.
 dia :: QDiagram SVG V2 Double Any -> Graphic
-dia = fromSvg . renderDia SVG (SVGOptions (mkWidth 250) Nothing (Text.pack "") [] True)
+dia = fromSvg . renderDia SVG (SVGOptions sz Nothing (Text.pack "") [] True)
+  where
+    sz = mkSizeSpec2D Nothing Nothing
 
 {-----------------------------------------------------------------------------
     Integration of the `Chart` package
 ------------------------------------------------------------------------------}
+chartX, chartY, chartScale :: Double
+chartScale = 1.3
+chartX = 640 / chartScale
+chartY = 480 / chartScale
+
 -- | Create a global environment for rendering charts.
 -- Essentially, this will load fonts.
 globalChartEnv :: Chart.DEnv Double
-globalChartEnv =
-    unsafePerformIO $ Chart.defaultEnv Chart.vectorAlignmentFns 640 480
+globalChartEnv = unsafePerformIO $
+    Chart.defaultEnv Chart.vectorAlignmentFns chartX chartY
 
 -- | Render a chart via SVG.
+--
+-- The rendered image has size 640 x 480 px.
 chart :: Chart.ToRenderable a => a -> Graphic
 chart = dia
+    . scale chartScale
     . fst . Chart.runBackend globalChartEnv
-    . flip Chart.render (640, 480) . Chart.toRenderable
+    . flip Chart.render (chartX, chartY) . Chart.toRenderable
 
 {-----------------------------------------------------------------------------
     Integration of the `QuickCheck` package
